dfc077721b8ba214e40e4ed459908257b4295a9d
Apratim <apratimr55@gmail.com>
2021-08-26 05:17:16 +0530

    added process and padding class

--- src.py
+++ src.py
@@ -6,7 +6,7 @@ class convert:
     _char_array = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
 
     @staticmethod
-    def b64encode(string1):
+    def string_to_b64(string1):
         """
         converts UTF-8 string to URL-Safe Base64 string
         """
@@ -20,7 +20,7 @@ class convert:
             raise Exception("Invalid data type entered")
 
     @staticmethod
-    def b64decode(string1):
+    def b64_to_string(string1):
         """
         converts only Base64 encoded string back to UTF-8 string
         """
@@ -40,27 +40,54 @@ class convert:
             raise Exception("Invalid Data Type entered")
 
     @staticmethod
-    def array_encode(string1):
+    def string_to_array(string1):
         """
         converts UTF-8 string to int array with value range 0-63
         """
-        string1 = convert.b64encode(string1)
+        string1 = convert.string_to_b64(string1)
         array1 = np.zeros((len(string1)), dtype=np.uint8)
         for temp in range(len(string1)):
             array1[temp] = convert._char_array.index(string1[temp])
         return array1
 
     @staticmethod
-    def array_decode(array1):
+    def array_to_string(array1):
         """
         converts int array with value range 0-63 back to UTF-8 string
         """
         string1 = ""
         for temp in array1:
             string1 += convert._char_array[temp]
-        string1 = convert.b64decode(string1)
+        string1 = convert.b64_to_string(string1)
         return string1
 
 
+class padding:
+    @staticmethod
+    def padwith(array1, padder, amount):
+        temp = [padder for _ in range(amount)]
+        array1 = np.append(array1, temp)
+        return array1
+
+    @staticmethod
+    def padtomultipleof(array1, padder, amount):
+        topad = amount - (len(array1) % amount)
+        temp = [padder for _ in range(topad)]
+        array1 = np.append(array1, temp)
+        return array1
+
+
 class process:
-    pass
+    @staticmethod
+    def XOR_arrays(array1, array2):
+        if len(array1) == len(array2):
+            return [x ^ y for x, y in zip(array1, array2)]
+        else:
+            raise Exception("Invalid lengths of array entered")
+
+    @staticmethod
+    def shift(array1, amount, direction):
+        if direction == "l":
+            return np.roll(array1, -amount)
+        elif direction == "r":
+            return np.roll(array1, amount)
--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import src as sc
 
-print(temp := sc.convert.array_encode('🇮🇳asdn98qn-as9dn--a sd9adabf-9sd'))
+# print(temp := sc.convert.string_to_array("🇮🇳asdn98qn-as9dn--a sd9adabf-9sd"))
 
-print(sc.convert.array_decode(temp))
+# print(sc.convert.array_to_string(temp))
 
-print(sc.convert._char_array)
+# print(sc.convert._char_array)
+
+temp1 = sc.convert.string_to_array("123")
+temp2 = sc.convert.string_to_array("321")
+
+temp3 = sc.process.XOR_arrays(temp1, temp2)
+temp3 = sc.padding.padtomultipleof(temp3, 0, 10)
+temp3 = sc.process.shift(temp3, 3, "l")
+print(temp3)
